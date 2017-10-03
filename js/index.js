dd83c3c03eb8286b1a06688e6abed22447d67be9
Luis Ruiz <la.ruiz967@uniandes.edu.co>
2017-10-03 15:35:21 -0500

    Primer avance en la vista inicial

---
+++ js/index.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+  alert("Funciona JQuery");
+
+  $('a[href*=#]').click(function(event){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    event.preventDefault();
+  });
+
+});
